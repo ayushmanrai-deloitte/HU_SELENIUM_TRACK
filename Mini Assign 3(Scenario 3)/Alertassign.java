a2bd13fb2c597e713db715465efb1cbee420be4e
Rai <ayurai@deloitte.com>
2022-03-24 12:38:39 +0530

    Mini Assignment 3 Scenario 3

---
+++ Mini Assign 3(Scenario 3)/Alertassign.java
@@ -0,0 +1,31 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.Point;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+public class Alertassign {
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "C:\\chromedriver.exe");
+        WebDriver dri = new ChromeDriver();
+        dri.manage().window().maximize();// browser window maximizes
+        dri.get("https://the-internet.herokuapp.com/");//goes to url
+        Thread.sleep (2000);
+        dri.navigate().to("https://the-internet.herokuapp.com/javascript_alerts");//Click on java script
+        Thread.sleep (2000);
+        dri.findElement(By.xpath("//*[@id=\"content\"]/div/ul/li[3]/button")).click();//click on js prompt
+        Thread.sleep (3000);
+        String expected = "You entered: Ex-Test";//alert input
+        dri.switchTo().alert().sendKeys("Ex-Test");
+        Thread.sleep (3000);
+        dri.switchTo().alert().accept();
+        String input = dri.findElement(By.xpath("//*[@id=\"content\"]/div/h4")).getText();
+        if (expected.equals(input)== true)
+        {
+            System.out.println("VERIFIED");
+        }else
+        {
+            System.out.println("NOT VERIFIED");
+        }
+
+
+    }
+}
